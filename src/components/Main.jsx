b125ba3af2a398d1994084c7a812c94705dcdc41
A.M <andrej2000aa@gmail.com>
2023-07-24 20:00:07 +0200

    t5

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,23 +2,17 @@ import React from 'react'
 import '../assets/css/main.scss'
 
 
-import Zoom from 'react-reveal/Zoom';
-import Slide from 'react-reveal/Slide';
-import config from 'react-reveal/globals';
 
 
 
 const Main = () => {
-    config({ ssrFadeout: true });
     return (
         <>
             <div className='main'>
 
                 
 
-                <Zoom >
                     <h1>Proba #1</h1>
-                </Zoom>
 
               
 
@@ -28,9 +22,7 @@ const Main = () => {
 
             <div className='main2'>
 
-            <Slide bottom >
                     <h1>Natasaaaa</h1>
-                </Slide>
 
         
                 
