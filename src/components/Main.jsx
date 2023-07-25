671dee69269d51ddc574b058982f1d283b251d7f
A.M <andrej2000aa@gmail.com>
2023-07-25 18:21:02 +0200

    novi scroll reveal

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,14 @@ import { Link } from 'gatsby'
 import '../assets/css/main.scss'
 
 
-
+import ScrollRevealTitle from '../components/ScrollReveal';
 
 
 const Main = () => {
+
+
+
+
     return (
         <>
             <div className='main'>
@@ -16,9 +20,11 @@ const Main = () => {
                     <h1>Proba #1</h1>
 
 
-              <Link to='/kontakt'><h3>Kontakt strana</h3></Link>
+              <Link to='/kontakt' ><h3>Kontakt strana</h3></Link>
 
                 <h1>BlaBlaBla</h1>
+
+              <ScrollRevealTitle>Scroll Reveal Naslov</ScrollRevealTitle>
                 
             </div>
 
@@ -31,7 +37,16 @@ const Main = () => {
                         <h2>Natasa!</h2>
                     </div>
 
-        
+              {/*<AnimateIn distance={30} triggerOnce={false}>*/}
+              {/*  <h1>Hi Mom!</h1>*/}
+              {/*</AnimateIn>*/}
+
+              <h1  >Hi Mom!</h1>
+
+              <ScrollRevealTitle>
+                <h1>Zdravo Naticio moj</h1>
+              </ScrollRevealTitle>
+
                 
             </div>
         </>
